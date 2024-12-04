f9266035e46273cff521c13810b77cc8e68e9501
Suresh Warrier <48576944+subabu2503@users.noreply.github.com>
2024-12-04 16:12:32 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,12 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+  # Update this block with the location of your terraform state file
   backend "azurerm" {
     resource_group_name  = "rg-tf-backend"
     storage_account_name = "sbwstaccounttfbackend"
@@ -7,3 +15,4 @@ terraform {
     use_oidc             = true
   }
 }
+
