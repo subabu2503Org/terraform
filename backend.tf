d74c47da659e78e8da4bed48bb43c50f13aeff11
Suresh Warrier <48576944+subabu2503@users.noreply.github.com>
2024-12-04 17:06:02 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
   # Update this block with the location of your terraform state file
   backend "azurerm" {
     resource_group_name  = "rg-tf-backend"
