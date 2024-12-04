e023f3a9c984f4b8493ea66b15a9a94568f0baa6
Suresh Warrier <48576944+subabu2503@users.noreply.github.com>
2024-12-04 15:25:20 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
 
 resource "azurerm_resource_group" "default" {
