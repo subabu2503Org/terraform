cda45c1b63bbca95ebdb73372cec32cb60a4705f
Suresh Warrier <sureshbwarrier@gmail.com>
2025-02-12 20:24:17 -0600

    added front door

---
+++ frontdoor-with-waf/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">=1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "rg-tf-backend"
+    storage_account_name = "sbwstaccounttfbackend"
+    container_name       = "terraform-state-github"
+    key                  = "terraform-state-github"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
