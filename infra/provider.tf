f79bf172272258cc35f5680c37645bd34c07b09a
Suresh Warrier <48576944+subabu2503@users.noreply.github.com>
2025-02-20 19:08:08 -0600

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -11,7 +11,7 @@ terraform {
   # Update this block with the location of your terraform state file
   backend "azurerm" {
     resource_group_name  = "rg-tf-backend"
-    storage_account_name = "sbwstaccounttfbackend"
+    storage_account_name = ${{ secrets.STATE_FILE_STORAGE_ACCOUNT_NAME }}
     container_name       = "terraform-state-github"
     key                  = "terraform-state-github"
     use_oidc             = true
