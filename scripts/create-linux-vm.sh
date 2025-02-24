23a4b817bbc2d480fbdc8cc2bd3debc1192b0d21
Suresh Warrier <48576944+subabu2503@users.noreply.github.com>
2025-02-24 11:12:58 -0600

    Update create-linux-vm.sh

--- scripts/create-linux-vm.sh
+++ scripts/create-linux-vm.sh
@@ -2,10 +2,10 @@ pwd
 ls -lta
 rg="rg-linux-vm"
 echo $rg
-az group create  --location canadacentral --name $rg
+#az group create  --location canadacentral --name $rg
 keyvault_name="sbwlinuxvmcertkv1" 
 echo $keyvault_name
-az keyvault create --resource-group rg-linux-vm --name $keyvault_name --location canadacentral --enabled-for-deployment   
+#az keyvault create --resource-group rg-linux-vm --name $keyvault_name --location canadacentral --enabled-for-deployment   
 echo "keyvault certificate list-"
 az keyvault certificate list --vault-name $keyvault_name
 echo "az vm create-"
